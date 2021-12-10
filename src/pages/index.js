fc1be801c4923cfb2917e2992f4943752364905c
victor <bodegaflamenca666@gmail.com>
2021-12-10 23:17:34 +0100

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react'
 import Sidebar from "../components/Sidebar";
 import NavBar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
-import { TeamCards } from "../components/TeamCard";
-import InfoSection from "../components/Sections";
-import Picture1 from "../images/logoAirlines.png";
-import Picture2 from "../images/rees_logo.jpg";
+import InfoSection from "../components/Sections/InfoSection";
 import { motion } from "framer-motion";
 import { animationOne, transition } from "../animations";
 import PartnerSmallCards from "../components/PartnersHome/PartnerSmallCards";
-import Rocket2 from "../animations/Rocket2";
+import { HomeNews, HomePartners, HomeTeam, HomeGaming, HomeNFT } from "../components/Sections/Data.js";
+
 
 
 const Home = () => {
@@ -25,11 +23,12 @@ const Home = () => {
             <Sidebar isOpen={isOpen} toggle={toggle}/>
             <NavBar />
             <HeroSection />
-            {/* <InfoSection image={Picture1} title="DAIR" desc="Discover the token" buttonText="PRESS ME"/> */}
-            <Rocket2 />
+            <InfoSection {...HomeNews}/>
+            <InfoSection {...HomeTeam}/>
+            <InfoSection {...HomeGaming}/>
+            <InfoSection {...HomePartners}/>
+            <InfoSection {...HomeNFT}/>
             <PartnerSmallCards />
-            <InfoSection image={Picture2} title="REES" desc="Discover the token" buttonText="PRESS ME"/>
-            <TeamCards/>
         </motion.div>
     );
 }
