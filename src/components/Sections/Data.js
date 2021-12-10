d5ab6ddb292b8bc7a5eb515a1c339510c254ec6a
victor <bodegaflamenca666@gmail.com>
2021-12-10 19:46:25 +0100

    second commit

--- src/components/Sections/Data.js
+++ src/components/Sections/Data.js
@@ -1,16 +1,92 @@
-export const homeObjOne = {
-    id: "partners",
+import ImageNews from "../../images/news.png";
+import ImageTeam from "../../images/team.png";
+import ImageNft from "../../images/nftSvg.svg";
+import Image4 from "../../images/gaming.png";
+import ImagePartners from "../../images/partnersSvg.svg";
+
+export const HomeNews = {
+    id: "news",
     lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
+    lightText: false,
+    lightTextDesc: false,
     topLine: "Latest News",
-    headLine: "Get access to the latest Destiny World & its associates news",
+    //headline: "Get access to the latest Destiny World & its associates news",
     description: "Destiny World is one of the most trending ecosystems connecting dApps and Communities. Get to know about us right here",
     buttonLabel: "Read the news",
     imgStart: false,
-    img: require("../../images/svg1.svg"),
+    img: ImageNews,
     alt: 'Rocket',
-    dark: true,
+    dark: false,
+    primary: true,   
+    darkText: false
+};
+
+export const HomePartners = {
+    id: "partners",
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "Our Partners",
+    //headline: "Get access to the latest Destiny World & its associates news",
+    description: "Destiny World relies on several trustful partners from different locations all around the world",
+    buttonLabel: "Meet our Partners",
+    imgStart: false,
+    img: ImagePartners,
+    alt: 'Rocket',
+    dark: false,
+    primary: true,   
+    darkText: false,
+    colorText: true,
+};
+
+export const HomeTeam = {
+    id: "team",
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "Destiny's Team",
+    //headline: "Get access to the latest Destiny World & its associates news",
+    description: "These are the people that have made Destiny World possible",
+    buttonLabel: "Meet the Team",
+    imgStart: false,
+    img: ImageTeam,
+    alt: 'Rocket',
+    dark: false,
+    primary: true,   
+    darkText: false,
+    colorText: false,
+};
+
+export const HomeGaming = {
+    id: "gaming",
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "Gaming Zone",
+    //headline: "Get access to the latest Destiny World & its associates news",
+    description: "Where crypto, Sports and Casino games meet",
+    buttonLabel: "Take a Ride",
+    imgStart: false,
+    img: Image4,
+    alt: 'Rocket',
+    dark: false,
+    primary: true,   
+    darkText: false
+};
+
+export const HomeNFT = {
+    id: "nft",
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: "NFT's",
+    //headline: "Get access to the latest Destiny World & its associates news",
+    description: "Destiny World is associated with several NFT platforms and artists such as Wayne Sheppard",
+    buttonLabel: "Enjoy the Art",
+    imgStart: false,
+    img: ImageNft,
+    alt: 'Rocket',
+    dark: false,
     primary: true,   
     darkText: false
 };
--- src/components/Sections/InfoSection.js
+++ src/components/Sections/InfoSection.js
@@ -1,19 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Button } from "@material-ui/core";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 import { Section, Container } from "./Elements";
 
+import {InfoContainer, InfoWrapper, InfoRow, Column1, TextWrapper, TopLine, Heading, Subtitle, Column2, ImgWrap, Img, alt} from "./Elements";
+
+const StyledButton = styled(Button)`
+    &&&{
+      border: 1px solid gold;
+      color: gold;
+      margin-left: 120px;
+      
+
+      @media screen and (max-width: 768px) {
+        
+        margin-top: -20px;
+        display: block;
+        margin-left: auto;
+        margin-right:auto;  
+      }
+
+      &:hover{
+          background-color: gold;
+          font-weight: bold;
+          transform: scale(1.1);
+      }
+    }
+`;
 
 
-
-
-
-const InfoSection = ({ image, title, desc }) => {
+const InfoSection = ({ lightBg, imgStart, topLine, lightText, darkText, buttonLabel, img, alt, id, headline, description, colortext }) => {
     return (
-       <Section image={image}>
-            <Container>
-                <h1>{title}</h1>
-                <p>{desc}</p>
-            </Container>
-       </Section>
+       <>
+        <InfoContainer lightBg={lightBg} id={id}>
+            <InfoWrapper>
+                <InfoRow imgStart={imgStart}>
+                    <Column1>
+                        <TextWrapper>
+                            <TopLine colorText={colortext}>{topLine}</TopLine>
+                            <Heading lightText={lightText}>{headline}</Heading>
+                            <Subtitle darkText={darkText}>{description}</Subtitle>
+                            <StyledButton to="home">{buttonLabel}</StyledButton>
+                        </TextWrapper>
+                    </Column1>
+                    <Column2>
+                        <ImgWrap>
+                            <Img src={img} alt={alt}/>
+                        </ImgWrap>
+                    </Column2>
+                </InfoRow>
+            </InfoWrapper>
+        </InfoContainer>
+       </>
            
     );
 }
