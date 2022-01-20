b7850b7e9d913645c702f00c59acd49cb2da086b
victor <bodegaflamenca666@gmail.com>
2022-01-20 09:45:18 +0100

    gamingPage

---
+++ src/components/NavBar/GamingNavBar.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
+import { Link as LinkS } from "react-scroll";
+import { motion, AnimatePresence } from "framer-motion";
+import { Nav, NavBarContainer, NavLogo, NavMenu, NavItem, NavbarLink, HiddenNav, MobileIcon2 } from "./Elements";
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from "@material-ui/core";
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import { animateScroll as scroll } from "react-scroll";
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import GroupsIcon from '@mui/icons-material/Groups';
+import PriceCheckIcon from '@mui/icons-material/PriceCheck';
+import MapIcon from '@mui/icons-material/Map';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
+
+
+
+
+const ScrollIcon = styled(ArrowCircleUpIcon)`
+    color: gold;
+    transition: 0.8s all ease;
+    top: 600px;
+`;
+
+const SwitchIcon = styled.div`
+    color: gold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    &:hover{
+       transform: scale(1.2);
+       transition: 0.4s all ease;
+   }
+`;
+
+const StyledMenuIcon = styled(MenuIcon)`
+    &&&{
+        color: gold;
+        @media screen and (min-width: 1100px){
+            display: none;
+        }
+    }
+`;
+
+
+
+
+
+const GamingNavbar = (props) => {
+
+    const [theme, setTheme] = useState("dark");
+
+    function changeTheme() {
+        if(props.theme === "light") {
+            props.setTheme("dark");
+        } else {
+            props.setTheme("light");
+        }
+    }
+
+    const icon = props.theme === "light" ? <DarkModeIcon /> : <LightModeIcon /> 
+
+    
+
+    const [scrollNavDown, setScrollNavDown] = useState(false);
+
+    const changeNavDown = () => {
+        if(window.scrollY >= 50) {
+            setScrollNavDown(true)
+        } 
+        else {
+            setScrollNavDown(false)
+        }
+    }
+
+    useEffect(() => {
+       window.addEventListener('scroll', changeNavDown) 
+    }, []);
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+    
+
+    return (
+        <>
+            
+            <Nav scrollNavDown={scrollNavDown}>
+                    
+                    <NavLogo to="/HomeNoLoader" >Destiny Airlines</NavLogo>
+                   
+                    <IconButton onClick={changeTheme}><SwitchIcon >
+                        {icon}
+                    </SwitchIcon></IconButton>
+              
+                    <MobileIcon2 scrollNavDown={scrollNavDown}>
+                        <IconButton onClick={toggleHome}>
+                            <ScrollIcon />
+                        </IconButton>
+                    </MobileIcon2> 
+                    <NavMenu>
+                        <NavItem>
+                            <NavbarLink to="ceo" smooth={true} duration={500} spy={true} exact="true" >DAVID</NavbarLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavbarLink to="cio" smooth={true} duration={500} spy={true} exact="true" >FILIP</NavbarLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavbarLink to="dev1" smooth={true} duration={500} spy={true} exact="true" >VICTOR</NavbarLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavbarLink to="dev2" smooth={true} duration={2000} spy={true} exact="true" >BASHKAR</NavbarLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavbarLink to="dev3" smooth={true} duration={500} spy={true} exact="true" >ABHIK</NavbarLink>
+                        </NavItem>
+                    </NavMenu>
+                
+            </Nav>
+            
+            
+        </>
+    );
+};
+
+export default GamingNavbar;
--- src/pages/GamingPage.js
+++ src/pages/GamingPage.js
@@ -1,25 +1,233 @@
-import React, { useState } from 'react'
-
-import AnimatedSection from "../components/Sections";
+import React, {useState, useContext} from 'react';
+import styled from "styled-components";
+import { Button } from "@material-ui/core";
 import { motion } from "framer-motion";
+import { Link as LinkR } from "react-router-dom";
 import { animationThree } from "../animations";
+import { ThemeProvider } from "styled-components";
+import { themes } from "../components/Themes";
+import GamingNavBar from "../components/NavBar/GamingNavBar";
+import TelegramIcon from '@mui/icons-material/Telegram';
+import {IconButton} from "@material-ui/core";
+
+import BotPhone1 from "../images/BotPhone1.png";
+import BotPhone2 from "../images/BotPhone2.png";
+import BotPhone3 from "../images/BotPhone3.png";
+import GamingPhone1 from "../images/GroupPhone1.png";
+import GamingPhone2 from "../images/GroupPhone2.png";
+import GamingPhone3 from "../images/GroupPhone3.png";
+
+
+const Container = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background: ${props => props.theme.body};
+
+    @media screen and (max-width: 1100px){
+        flex-direction: column;
+    }
+`;
+
+const Image = styled(motion.img)`
+    width: 200px;
+    height: auto;
+    position: absolute;
+    z-index: 5;
+`;
+
+const ColumnLeft = styled.div`
+    flex-shrink: 0;
+    flex-basis: 50%;
+    background: ${props => props.theme.horizontalRed};
+    width: 50%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    
+
+    @media screen and (min-width: 1100px){
+        padding-left: 100px;
+        padding-bottom: 50px;
+    }
+
+    @media screen and (max-width: 1100px){
+        background: ${props => props.theme.verticalRed};
+        width: 100vw;
+        
+        
+        
+    }
+`;
+const ColumnRight = styled.div`
+    background: ${props => props.theme.horizontalRed};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+    height: 100vh;
+    padding: 10px 100px;
+    
+    text-align: center;
+    @media screen and (max-width: 1100px){
+        height: 60vh;
+        width: 100vw;
+        align-items: center;
+        justify-content: center;
+        padding: 10px 20px;
+        padding-top: 100px;
+        background: ${props => props.theme.verticalRed};
+        
+    }
+    @media screen and (min-width: 1100px){
+        padding-right: 200px;
+        
+    }
+`;
+
+
+
+const SectionImage = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        width: 50%;
+    }
+    @media screen and (min-width: 1000px){
+        img{
+           width: 25vw;
+        }
+    }
+`;
+
+
+
+const TopLine = styled.p`
+    color: ${props => props.theme.text};
+    font-size: 20px;
+    font-weight: 700;
+    margin-bottom: 30px;
+    font-family: "Comfortaa";
+    @media screen and (max-width: 768px){
+        font-size: 18px;
+        margin-bottom: 20px;
+    }
+
+    
+`;
+
+const Heading = styled.h1`
+    font-size: 48px;
+    font-weight: 600;
+    color: ${props => props.theme.text};
+    
+
+    @media screen and (max-width: 480px){
+        font-size: 32px;
+        margin-bottom: 20px;
+    }
+`;
+
+const StyledButton = styled(LinkR)`
+    &&& {
+        color: ${props => props.theme.text};
+        border: 1px solid ${props => props.theme.text};
+        height: 40px;
+        width: 150px;
+        margin-bottom: 30px;
+        text-decoration: none;
+        font-size: 18px;
+        border-radius: 10px;
+        padding: 10px 10px;
+    }
+`;
+
+const IconContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+    transform: scale(1.5);
+
+    @media screen and (max-width: 1100px){
+        height: 50px;
+        transform: scale(1.2);
+    }
+`;
+
+const Telegram = styled(TelegramIcon)`
+    color: blue;
+    
+`;
 
 
 
 const GamingPage = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
+    const [theme, setTheme] = useState("dark");
 
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    }
+    const TelegramBotLink = "https://t.me/PredictCryptoBot";
+    const TelegramGroupLink = "https://t.me/dwbetsandpredictions";
 
     return (
+        <ThemeProvider theme={themes[theme]}>
         <motion.div initial="out" animate="end" exit="out" variants={animationThree}>
+            <GamingNavBar />
+            <Container >
+            <ColumnLeft>
+                <Image src={BotPhone3} alt="planet" 
+                whileHover={{scale: 1.5, zIndex: 100}}
+                whileTap={{scale: 1.5, zIndex: 100}}
+                initial={{x: 100, y:40}} /> 
+                <Image src={BotPhone2} alt="planet" 
+                whileHover={{scale: 1.5, zIndex: 100}}
+                whileTap={{scale: 1.5, zIndex: 100}}
+                initial={{x: -100, y:40}} /> 
+                <Image src={BotPhone1} alt="planet" 
+                whileHover={{scale: 1.5, zIndex: 100}}
+                whileTap={{scale: 1.5, zIndex: 100}}
+                initial={{x: 0, y:40, scale:1.2}} /> 
+            </ColumnLeft>
+            <ColumnRight>
+            
+                <TopLine>Join our Telegram Bot and put your crypto into play. Sport Bets, Casino and Dice Games, BTC and ETH price predictions, and many more</TopLine>
+                <Heading>TELEGRAM BOT</Heading>
+                <IconContainer>
+                <IconButton onClick={event => window.open(TelegramBotLink, "_blank")}><Telegram /></IconButton>
+                </IconContainer>
+            </ColumnRight>
+        </Container>
+        <Container >
+            <ColumnLeft>
+                <Image src={GamingPhone1} alt="planet" 
+                whileHover={{scale: 1.5, zIndex: 100}}
+                initial={{x: 100, y:40}} /> 
+                <Image src={GamingPhone2} alt="planet" 
+                whileHover={{scale: 1.5, zIndex: 100}}
+                initial={{x: -100, y:40}} /> 
+                <Image src={GamingPhone3} alt="planet" 
+                whileHover={{scale: 1.5, zIndex: 100}}
+                initial={{x: 0, y:40, scale:1.2}} /> 
+            </ColumnLeft>
+            <ColumnRight>
+            
+                <TopLine>Keep track of your bet results in our Telegram Sports Group, or simply discuss about sports. Live Results, Sport Latest News, Videos, and many more</TopLine>
+                <Heading>TELEGRAM SPORTS GROUP</Heading>
+                <IconContainer>
+                <IconButton onClick={event => window.open(TelegramGroupLink, "_blank")}><Telegram /></IconButton>
+                </IconContainer>
+            
+            </ColumnRight>
+        </Container>
             
             
-            <AnimatedSection />
         </motion.div>
+        </ThemeProvider>
     )
 }
 
